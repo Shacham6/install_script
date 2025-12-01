c6fbf62e9a4a1d63b37daa71c7f4c1d7d93a66cb
Shacham Ginat <shach@Shachams-MacBook-Pro.local>
2025-12-01 09:21:52 +0200

    A bunch of good things, yes

--- install_script.sh
+++ install_script.sh
@@ -13,6 +13,26 @@ ostype() {
 	fi
 }
 
+_is_on_mac() {
+	[ "$(ostype)" = "mac" ]
+}
+
+_on_mac() {
+	if _is_on_mac; then
+		${*}
+	fi
+}
+
+_is_on_arch() {
+	[ "$(ostype)" = "arch" ]
+}
+
+_on_arch() {
+	if [[ "$(ostype)" -eq "arch" ]]; then
+		${*}
+	fi
+}
+
 _which() {
 	if [[ "${#}" -eq 0 ]]; then
 		echo "_whitch requires an argument" >&2
@@ -33,6 +53,12 @@ _not_exists() {
 	return 0
 }
 
+_when_missing() {
+	if _not_exists ${1}; then
+		${*:2}
+	fi
+}
+
 _screen_fence() {
 	local width=$(tput cols)
 	printf "\n"
@@ -51,7 +77,7 @@ red() {
 }
 
 msg_installing() {
-	printf " ## ----> Installing $(yellow ${1})...\n"
+	printf " ## ----> Installing $(yellow ${1})..."
 }
 
 msg_install_ok() {
@@ -60,15 +86,57 @@ msg_install_ok() {
 
 msg_install_err() {
 	printf " ## ----> $(yellow ${1}) installation has $(red failed)\n"
+	return 1
 }
 
 msg_fenced() {
 	_screen_fence
 	printf "${*}"
-	printf "\n"
 	_screen_fence
 }
 
-msg_installing "git"
-msg_install_ok "git"
-msg_install_err "git"
+# nl is just newline
+nl() {
+	printf "\n"
+}
+
+if _is_on_mac; then  # Making the brew installs non-interactive
+	export NONINTERACTIVE=1
+fi
+
+# Installing Brew on mac.
+
+if _is_on_mac; then
+	msg_installing brew
+	_when_missing brew /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	msg_fenced "you might need to do extra things like add this in path"
+	msg_install_ok brew
+fi
+
+# Intalling yay on arch.
+
+if _is_on_arch; then
+	msg_installing yay
+	sudo pacman -Syu --noconfirm
+	sudo pacman -S --noconfirm --needed base-devel git
+
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+
+	makepkg -si
+
+	_which yay && msg_install_ok yay || msg_install_err yay
+fi
+
+# Installing git.
+
+if _not_exists git; then
+	msg_installing git
+	_on_mac brew install git && msg_install_ok git || msg_install_err git
+
+	if _is_on_arch; then
+		sudo pacman -Syu --no-confirm
+		sudo pacman -S --no-confirm git
+	fi
+fi
+
